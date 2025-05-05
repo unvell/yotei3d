747ab0fc65d9c45cb52ce0648089f96d1cfab469
Jingwood <dujid0@gmail.com>
2025-05-05 12:45:20 +0900

    update deploy settings

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,16 @@ import string from 'vite-plugin-string';
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
+import { globSync } from 'glob'
 
 const isExampleSite = process.env.BUILD_EXAMPLES_SITE === 'true';
+
+// HTMLファイルを自動的に取得
+const htmlInputs = {};
+globSync('examples/*.html').forEach(file => {
+  const name = path.basename(file, '.html');
+  htmlInputs[name] = path.resolve(__dirname, file);
+});
 
 export default defineConfig({
   root: isExampleSite ? 'examples' : 'examples',
@@ -16,6 +24,9 @@ export default defineConfig({
     // examples site
     outDir: '../examples-dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: htmlInputs, // ← 自動的に取得したHTMLファイル群を指定
+    },
     plugins: [
       tailwindcss(),
     ],
